770ec718318a25878fb371b17afe33deea28f1d1
MANISH YADAV <manishyadav.nd@gmail.com>
2017-10-30 18:33:10 +0530

    update 2

--- Fibonacci_recursion.c
+++ Fibonacci_recursion.c
@@ -10,3 +10,28 @@ return fibonacci(n-1) + fibonacci(n-2);
 }
 
 
+
+
+int main ()
+{
+int n = 40;
+
+clock_t begin, end;
+
+double time_spent;
+
+begin = clock();
+
+printf("%d\n", fibonacci(n));
+
+end = clock();
+
+time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+
+printf("\nTime Taken %lf ", time_spent);
+
+getchar();
+return 0;
+}
+
+
